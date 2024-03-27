acf4d2e6bbc6650d5ddcd49f109585256ad1dda3
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2024-03-27 17:01:47 +0500

    integrated meta and avatar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,26 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Avatar from "../components/Avatar";
+import Meta from '../components/Meta';
+import { ViewGridIcon } from '@heroicons/react/solid';
 
 export default function Home() {
   return (
-    <h1>Lets build google with next js and tailwind</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Meta title="Google 2.0 | Ahsan Nazeef" />
+
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+        <div className="flex space-x-4 items-center">
+          <p className="link">About</p>
+          <p className="link">Store</p>
+        </div>
+
+        <div className="flex space-x-4 items-center">
+          <p className="link">Gmail</p>
+          <p className="link">Images</p>
+
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
+          <Avatar url="/avatar.png" />
+        </div>
+      </header>
+    </div>
   );
 }
