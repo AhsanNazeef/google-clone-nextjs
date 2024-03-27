7400fac29228e5ea7d1e09c93b2505c45b44e31d
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2024-03-27 17:01:08 +0500

    fixed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['www.google.co.uk'],
+  },
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    googleApiKey: process.env.GOOGLE_API_KEY,
+    googleContextKey: process.env.GOOGLE_CONTEXT_KEY,
+    customSearchApi: 'https://www.googleapis.com/customsearch/v1',
+  }
 };
 
 export default nextConfig;
