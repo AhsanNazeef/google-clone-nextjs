6178d2d1c3bff5fcc9cea2985998dcd1d1f7f817
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2024-03-27 17:32:20 +0500

    completed implementation of main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,35 @@ import Meta from '../components/Meta';
 import Footer from '../components/Footer';
 import { MicrophoneIcon, SearchIcon, ViewGridIcon, XIcon } from '@heroicons/react/solid';
 import Image from "next/image";
+import classNames from "classnames";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const [query, setQuery] = useState('');
+
+  const onSearch = (e: any) => {
+    e.preventDefault();
+
+    if (!query) return;
+
+    router.push({
+      pathname: '/search',
+      query: {
+        q: query,
+      },
+    });
+  };
+
+  const onQuery = (e: any) => {
+    setQuery(e.target.value);
+  };
+
+  const onClear = () => {
+    setQuery('');
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Meta title="Google 2.0 | Ahsan Nazeef" />
@@ -35,19 +62,29 @@ export default function Home() {
         <div className="flex w-full mt-5 hover:shadow-lg max-w-md focus-within:shadow-lg rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
           <SearchIcon className="h-5 mr-3 text-gray-500" />
           <input
+            value={query}
+            onChange={onQuery}
             type="text"
             className="focus:outline-none flex-grow"
           />
           {
+          query && 
           (
             <XIcon
+              onClick={onClear}
               className="h-5 ml-3 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
             />
           )}
+          <MicrophoneIcon
+            className={classNames('text-gray-500 h-5 hidden sm:inline-flex', {
+              'border-l pl-4 border-gray-300': query,
+            })}
+          />
         </div>
 
         <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
           <button type="submit" className="btn" 
+          onClick={onSearch}
           >
             Google Search
           </button>
