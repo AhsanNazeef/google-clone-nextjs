acb7f3ede9acb09eb69d1aa823120980ed60e504
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2024-03-27 17:29:00 +0500

    implemented main section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import Avatar from "../components/Avatar";
 import Meta from '../components/Meta';
 import Footer from '../components/Footer';
-import { ViewGridIcon } from '@heroicons/react/solid';
+import { MicrophoneIcon, SearchIcon, ViewGridIcon, XIcon } from '@heroicons/react/solid';
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -22,6 +23,39 @@ export default function Home() {
           <Avatar url="/avatar.png" />
         </div>
       </header>
+
+      <form className="flex flex-col items-center mt-20 flex-grow w-4/5">
+        <Image
+          src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          width={300}
+          height={100}
+          alt="google logo"
+        />
+
+        <div className="flex w-full mt-5 hover:shadow-lg max-w-md focus-within:shadow-lg rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input
+            type="text"
+            className="focus:outline-none flex-grow"
+          />
+          {
+          (
+            <XIcon
+              className="h-5 ml-3 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
+            />
+          )}
+        </div>
+
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button type="submit" className="btn" 
+          >
+            Google Search
+          </button>
+          <button type="button" className="btn">
+            I&apos;m Feeling Lucky
+          </button>
+        </div>
+      </form>
 
       <Footer />
     </div>
